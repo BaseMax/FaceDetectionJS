044e327464561ece2dcc67063f4ff78d7d2fc99f
Max Base <maxbasecode@gmail.com>
2022-01-14 18:16:10 +0330

    fix script.js and upload model files

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const video = document.querySelector("video#video")
 
-Promise.all([
-	faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-	faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-	faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-	faceapi.nets.faceExpressionNet.loadFromUri("/models")
-]).then(startVideo)
-
 const startVideo = () => {
 	navigator.getUserMedia(
 		{ video: {} },
@@ -14,6 +7,13 @@ const startVideo = () => {
 		err => console.error(err)
 	)
 }
+
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+	faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+	faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+	faceapi.nets.faceExpressionNet.loadFromUri("/models")
+]).then(startVideo)
 
 video.addEventListener("play", () => {
 	const canvas = faceapi.createCanvasFromMedia(video)
