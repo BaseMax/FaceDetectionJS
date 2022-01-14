3ddd93cb54c72ad9d3403669586367a51c454e3c
Max Base <maxbasecode@gmail.com>
2022-01-14 18:13:58 +0330

    create basic script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const video = document.querySelector("video#video")
+
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+	faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+	faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+	faceapi.nets.faceExpressionNet.loadFromUri("/models")
+]).then(startVideo)
+
+const startVideo = () => {
+	navigator.getUserMedia(
+		{ video: {} },
+		stream => video.srcObject = stream,
+		err => console.error(err)
+	)
+}
+
+video.addEventListener("play", () => {
+	const canvas = faceapi.createCanvasFromMedia(video)
+	document.body.append(canvas)
+	const displaySize = {
+		width: video.width,
+		height: video.height
+	}
+	faceapi.matchDimensions(canvas, displaySize)
+	setInterval(async () => {
+		const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+		console.log(detections)
+		const resizedDetections = faceapi.resizeResults(detections, displaySize)
+		canvas.getContext("2d").clearReact(0, 0, canvas.width, canvas.height)
+		faceapi.draw.drawDetections(canvas, resizedDetections)
+		faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+		faceapi.draw.drawFaceExpresions(canvas, resizedDetections)
+	}, 100)
+})
